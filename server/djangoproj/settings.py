b887bca57406a37a7bbfc054d53a2423438d73b4
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2025-07-26 08:07:50 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://krishnanycku-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    (
+        'https://krishnanycku-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+        'proxy.cognitiveclass.ai'
+    )
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://krishnanycku-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    (
+        'https://krishnanycku-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
+        'proxy.cognitiveclass.ai'
+    )
 ]
 
 REST_FRAMEWORK = {
@@ -99,7 +105,10 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
         'NAME':
